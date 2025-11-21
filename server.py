c9ec8059c6bdc46e698e6e36ff575495e58474fe
solden <solden.stoll@gmail.com>
2025-11-20 17:48:17 -0800

    server progress

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 # Lab 3 Proxy Server
+from pstats import SortKey
 import sys
 import socket
 import struct
@@ -7,6 +8,9 @@ import threading
 # Constants
 DEBUG = True
 
+MIN_PORT = 1024
+MAX_PORT = 65535
+
 # Global list of sockets for cleanup
 sockets = []
 sockets_lock = threading.Lock()
@@ -14,14 +18,29 @@ sockets_lock = threading.Lock()
 def main(args):
     # Ensure correct arg length
     if (len(args) != 2):
-        Usage(args)
+        usage(args)
+        sys.exit()
     
     port = int(args[1])
 
-    
+    # Check for a valid port
+    if port < MIN_PORT or port > MAX_PORT:
+        print(f"Invalid port number: {port}")
+        sys.exit()
 
-def server(args):
-    
+    # Run the server
+    server(port)
+
+def server(port):
+    # Create a TCP listening port
+    listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    with sockets_lock:
+        sockets.append(listener)
+
+    listener.setsockopt(socket)
+
+def worker():
+    pass
 
 def usage(name):
     print(f"Usage: {name} port")
