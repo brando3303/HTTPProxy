1f6927747350c8560f9efd22706d773b08f62697
brandon <brando3303@gmail.com>
2025-11-21 16:49:08 -0800

    processing logic, start non connection

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ import socket
 import struct
 import threading
 
+import http_parser
+
 # Constants
 DEBUG = True
 
@@ -37,10 +39,90 @@ def server(port):
     with sockets_lock:
         sockets.append(listener)
 
-    listener.setsockopt(socket)
+    listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    if DEBUG:
+        print(f"Listening on port {port}")
+        print("Press Ctrl+C to stop the server")
+        
+    listener.bind(('', port))
+    listener.listen(5)
+    
+    try:
+        while True:
+            client_socket, client_address = listener.accept()
+            if DEBUG:
+                print(f"Accepted connection from {client_address}")
+            thread = threading.Thread(target=worker, args=(client_socket, client_address))
+            thread.daemon = True
+            thread.start()
+    except KeyboardInterrupt:
+        if DEBUG:
+            print("\nKeyboard interrupt received. Shutting down server...")
+    finally:
+        # Cleanup TODO: close everything
+        listener.close()
+        if DEBUG:
+            print("Server stopped")
 
-def worker():
+def worker(client_socket, client_address):
+    packet = b""
+
+    # first just get the header
+    while packet.find(b"\r\n\r\n") == -1:
+         packet += client_socket.recv(1024)
+
+    # process header TODO: allow other line endings
+    raw_header = packet.split(b"\r\n\r\n")[0]
+    header = http_parser.HTTPHeader(raw_header.decode())
+    data = header.generate_header()
+    
+    # Create TCP socket to destination server
+    dest_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    with sockets_lock:
+        sockets.append(dest_socket)
+
+    if header.get_header("Connection") == "keep-alive":
+        process_connection_request(client_socket, dest_socket)
+    else:
+        process_non_connection_request(client_socket, dest_socket, header, packet)
+
+    return
+
+
+
+def process_connection_request(client_socket, dest_socket):
     pass
+
+def process_non_connection_request(client_socket, dest_socket, header, packet):
+    dest = header.get_header("Host")
+
+    try:
+        # Parse host and port (default to 80 if not specified)
+        if ':' in dest:
+            host, port = dest.split(':')
+            port = int(port)
+        else:
+            host = dest
+            port = 80
+        
+        if DEBUG:
+            print(f"Connecting to {host}:{port}")
+        
+        dest_socket.connect((host, port))
+        
+        # TODO: Forward request to destination server
+        dest_socket.send(packet)
+
+        
+    except Exception as e:
+        if DEBUG:
+            print(f"Error connecting to {dest}: {e}")
+    finally:
+        dest_socket.close()
+        client_socket.close()
+
+    return
 
 def usage(name):
     print(f"Usage: {name} port")
